588c51999a67f7d1a32363f97b0584f498804288
devMontagnini <dev.montagnini@gmail.com>
2019-05-05 21:11:11 -0400

    Add files via upload

---
+++ modal-overlays-modal-overlays-module.js
@@ -0,0 +1,93 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["modal-overlays-modal-overlays-module"],{
+
+/***/ "./src/app/pages/modal-overlays/modal-overlays.module.ts":
+/*!***************************************************************!*\
+  !*** ./src/app/pages/modal-overlays/modal-overlays.module.ts ***!
+  \***************************************************************/
+/*! exports provided: ModalOverlaysModule */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "ModalOverlaysModule", function() { return ModalOverlaysModule; });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+/* harmony import */ var _theme_theme_module__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../@theme/theme.module */ "./src/app/@theme/theme.module.ts");
+/* harmony import */ var _modal_overlays_routing_module__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./modal-overlays-routing.module */ "./src/app/pages/modal-overlays/modal-overlays-routing.module.ts");
+/* harmony import */ var _modal_overlays_component__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./modal-overlays.component */ "./src/app/pages/modal-overlays/modal-overlays.component.ts");
+/* harmony import */ var _dialog_dialog_component__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./dialog/dialog.component */ "./src/app/pages/modal-overlays/dialog/dialog.component.ts");
+/* harmony import */ var _dialog_showcase_dialog_showcase_dialog_component__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ./dialog/showcase-dialog/showcase-dialog.component */ "./src/app/pages/modal-overlays/dialog/showcase-dialog/showcase-dialog.component.ts");
+/* harmony import */ var _nebular_theme__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! @nebular/theme */ "./node_modules/@nebular/theme/index.js");
+/* harmony import */ var _dialog_dialog_name_prompt_dialog_name_prompt_component__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(/*! ./dialog/dialog-name-prompt/dialog-name-prompt.component */ "./src/app/pages/modal-overlays/dialog/dialog-name-prompt/dialog-name-prompt.component.ts");
+/* harmony import */ var _window_window_component__WEBPACK_IMPORTED_MODULE_9__ = __webpack_require__(/*! ./window/window.component */ "./src/app/pages/modal-overlays/window/window.component.ts");
+/* harmony import */ var _window_window_form_window_form_component__WEBPACK_IMPORTED_MODULE_10__ = __webpack_require__(/*! ./window/window-form/window-form.component */ "./src/app/pages/modal-overlays/window/window-form/window-form.component.ts");
+/* harmony import */ var _toastr_toastr_component__WEBPACK_IMPORTED_MODULE_11__ = __webpack_require__(/*! ./toastr/toastr.component */ "./src/app/pages/modal-overlays/toastr/toastr.component.ts");
+/* harmony import */ var _popovers_popovers_component__WEBPACK_IMPORTED_MODULE_12__ = __webpack_require__(/*! ./popovers/popovers.component */ "./src/app/pages/modal-overlays/popovers/popovers.component.ts");
+/* harmony import */ var _popovers_popover_examples_component__WEBPACK_IMPORTED_MODULE_13__ = __webpack_require__(/*! ./popovers/popover-examples.component */ "./src/app/pages/modal-overlays/popovers/popover-examples.component.ts");
+/* harmony import */ var _tooltip_tooltip_component__WEBPACK_IMPORTED_MODULE_14__ = __webpack_require__(/*! ./tooltip/tooltip.component */ "./src/app/pages/modal-overlays/tooltip/tooltip.component.ts");
+
+
+// modules
+
+
+// components
+
+
+
+
+
+
+
+
+
+
+
+var COMPONENTS = [
+    _modal_overlays_component__WEBPACK_IMPORTED_MODULE_4__["ModalOverlaysComponent"],
+    _toastr_toastr_component__WEBPACK_IMPORTED_MODULE_11__["ToastrComponent"],
+    _dialog_dialog_component__WEBPACK_IMPORTED_MODULE_5__["DialogComponent"],
+    _dialog_showcase_dialog_showcase_dialog_component__WEBPACK_IMPORTED_MODULE_6__["ShowcaseDialogComponent"],
+    _dialog_dialog_name_prompt_dialog_name_prompt_component__WEBPACK_IMPORTED_MODULE_8__["DialogNamePromptComponent"],
+    _window_window_component__WEBPACK_IMPORTED_MODULE_9__["WindowComponent"],
+    _window_window_form_window_form_component__WEBPACK_IMPORTED_MODULE_10__["WindowFormComponent"],
+    _popovers_popovers_component__WEBPACK_IMPORTED_MODULE_12__["PopoversComponent"],
+    _popovers_popover_examples_component__WEBPACK_IMPORTED_MODULE_13__["NgxPopoverCardComponent"],
+    _popovers_popover_examples_component__WEBPACK_IMPORTED_MODULE_13__["NgxPopoverFormComponent"],
+    _popovers_popover_examples_component__WEBPACK_IMPORTED_MODULE_13__["NgxPopoverTabsComponent"],
+    _tooltip_tooltip_component__WEBPACK_IMPORTED_MODULE_14__["TooltipComponent"],
+];
+var ENTRY_COMPONENTS = [
+    _dialog_showcase_dialog_showcase_dialog_component__WEBPACK_IMPORTED_MODULE_6__["ShowcaseDialogComponent"],
+    _dialog_dialog_name_prompt_dialog_name_prompt_component__WEBPACK_IMPORTED_MODULE_8__["DialogNamePromptComponent"],
+    _window_window_form_window_form_component__WEBPACK_IMPORTED_MODULE_10__["WindowFormComponent"],
+    _popovers_popover_examples_component__WEBPACK_IMPORTED_MODULE_13__["NgxPopoverCardComponent"],
+    _popovers_popover_examples_component__WEBPACK_IMPORTED_MODULE_13__["NgxPopoverFormComponent"],
+    _popovers_popover_examples_component__WEBPACK_IMPORTED_MODULE_13__["NgxPopoverTabsComponent"],
+];
+var MODULES = [
+    _theme_theme_module__WEBPACK_IMPORTED_MODULE_2__["ThemeModule"],
+    _modal_overlays_routing_module__WEBPACK_IMPORTED_MODULE_3__["ModalOverlaysRoutingModule"],
+    _nebular_theme__WEBPACK_IMPORTED_MODULE_7__["NbDialogModule"].forChild(),
+    _nebular_theme__WEBPACK_IMPORTED_MODULE_7__["NbWindowModule"].forChild(),
+];
+var SERVICES = [];
+var ModalOverlaysModule = /** @class */ (function () {
+    function ModalOverlaysModule() {
+    }
+    ModalOverlaysModule = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["NgModule"])({
+            imports: MODULES.slice(),
+            declarations: COMPONENTS.slice(),
+            providers: SERVICES.slice(),
+            entryComponents: ENTRY_COMPONENTS.slice(),
+        })
+    ], ModalOverlaysModule);
+    return ModalOverlaysModule;
+}());
+
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=modal-overlays-modal-overlays-module.js.map
